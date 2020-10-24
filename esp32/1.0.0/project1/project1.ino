3731d74fe410db7fd8993837460217c106715a34
Stefx <ethan.emotion@gmail.com>
2020-10-24 15:46:43 +0200

    test ok, yellow tile implemented

--- esp32/1.0.0/project1/project1.ino
+++ esp32/1.0.0/project1/project1.ino
@@ -11,7 +11,7 @@ byte portB = (byte)PoweredUpHubPort::B;
 int velocity = 25;
 int connectedTrain=0;
 bool isVerbose = true;
-
+int colorInterval=5000;
 bool isInitialized = false;
 bool isSystemReady = false;
 
@@ -24,6 +24,7 @@ typedef struct {
   String hubAddress;
   int speed;
   int lastcolor;
+  unsigned long colorPreviousMillis;
   byte hubState;
   byte trainState;
 } Train;
@@ -34,8 +35,8 @@ typedef struct {
 
 //code  - hubobj - hubColor  -  hubAddress - speed - lastcolor - hubState (2 = off, 0=ready, 1=active)  - trainState(0 = stopped in Shed, 1 = forward, -1 = backwards, 2= stopped in hidden place) 
 Train myTrains[MY_TRAIN_LEN] = {
-   { "TA", &myTrainHub_TA, "Yellow" , "90:84:2b:04:a8:c5", velocity, 0, 2, 0}
-  ,{ "TB", &myTrainHub_TB, "Red", "90:84:2b:1c:be:cf", velocity, 0, 2, 0}  
+   { "TA", &myTrainHub_TA, "Yellow" , "90:84:2b:04:a8:c5", velocity, 0, 0, 2, 0}
+  ,{ "TB", &myTrainHub_TB, "Red", "90:84:2b:1c:be:cf", velocity, 0, 0 ,2, 0}  
 };
 
 
@@ -61,12 +62,10 @@ void hubButtonCallback(void *hub, HubPropertyReference hubProperty, uint8_t *pDa
             myTrains[idTrain].hubState=1;  
 
             _print("check ports... if needed sensor is already connected: ");
-            byte portForDevice = myHub->getPortForDeviceType((byte)DeviceType::COLOR_DISTANCE_SENSOR);
-            Serial.println(portForDevice, DEC); 
-          
+            byte portForDevice = myHub->getPortForDeviceType((byte)DeviceType::COLOR_DISTANCE_SENSOR);            
             if (portForDevice != 255){
               // activate hub button to receive updates                                  
-      			  Serial.println("activatePortDevice");
+      			  _println("activatePortDevice");
       			  myHub->activatePortDevice(portB, colorDistanceSensorCallback);
               delay(200);
             }
@@ -103,22 +102,25 @@ void colorDistanceSensorCallback(void *hub, byte portNumber, DeviceType deviceTy
 	// set hub LED color to detected color of sensor
 	// 5 verde ; 10 bianco; 9 rosso; 3 blu; 7 giallo
 	*/
+
+  
 	
   Lpf2Hub *myHub = (Lpf2Hub *)hub;
-  int idTrain = getHubIdByAddress(myHub->getHubAddress().toString().c_str());
-  _println("idTrain" + idTrain);
+  int idTrain = getHubIdByAddress(myHub->getHubAddress().toString().c_str());  
   if (idTrain==-1) return;  
-  if (myTrains[idTrain].hubState!=1) return;
+  if (myTrains[idTrain].hubState!=1) return;  
   
   if (deviceType == DeviceType::COLOR_DISTANCE_SENSOR){
     int color = myHub->parseColor(pData);
 
-     // exclude black (trail) and RED (table)  
-    if (myTrains[idTrain].lastcolor == color || color==0 || color==9 || color == 255) return;
+     // exclude black (trail) and RED (table)      
+    
+    if ((myTrains[idTrain].lastcolor == color || color==0 || color==9 || color == 255)) return;
     myTrains[idTrain].lastcolor = color;    
-    Serial.print("Color: ");
+    Serial.print("Color ");
+    Serial.print("Hub " + myTrains[idTrain].code + ":"); 
     Serial.println(COLOR_STRING[color]);
-    Serial.print("Color: ");
+    Serial.print("Color dec: ");
     Serial.println(color,DEC);    
 
     // set hub LED color to detected color of sensor and set motor speed dependent on color
@@ -132,7 +134,23 @@ void colorDistanceSensorCallback(void *hub, byte portNumber, DeviceType deviceTy
       myHub->setLedColor((Color)color);
       myHub->setBasicMotorSpeed(portA, myTrains[idTrain].speed);
     }
-    else if (color == (byte)Color::CYAN)
+
+    else if (color == (byte)Color::YELLOW) {
+              // TODO Check
+              _print("Stop & Invert");
+              myHub->setLedColor((Color)color);
+            
+              if (myTrains[idTrain].colorPreviousMillis == 0){
+                saveInterval(myTrains[idTrain].colorPreviousMillis);                      
+                myHub->stopBasicMotor(portA);
+                // invert
+                myTrains[idTrain].speed = -1 * myTrains[idTrain].speed;
+                //myTrains[idTrain].lastcolor = 107;    
+              }
+                                                                                                                              
+        }
+    
+    else if (color == (byte)Color::CYAN) //GREEN
     {
       myHub->setLedColor((Color)color);
 	    myTrains[idTrain].speed = -1 * myTrains[idTrain].speed;
@@ -154,6 +172,7 @@ void loop() {
 
     //if (!isInitialized) checkRemote();
     for (int i = 0; i < MY_TRAIN_LEN; i++){      
+      checkIntervalisExpired(i);
       if (! myTrains[i].hubobj->isConnected())scanHub(i);                
     }
     //if (isSystemReady) doMainCode();  
@@ -202,3 +221,25 @@ void _print(String text){
 void _println(String text){
   if(isVerbose) Serial.println(text);
 }
+
+
+void saveInterval(unsigned long &previousMillis){
+  _println("Setto timing");
+  previousMillis = millis();
+}  
+  
+void checkIntervalisExpired(int idTrain ){
+
+  Lpf2Hub *myTrain = myTrains[idTrain].hubobj;
+    
+  if (millis() - myTrains[idTrain].colorPreviousMillis > colorInterval && myTrains[idTrain].colorPreviousMillis>0){
+    
+    _println("Expired");      
+    myTrain->setBasicMotorSpeed(portA, myTrains[idTrain].speed);
+    myTrains[idTrain].colorPreviousMillis = 0;
+    
+  }else{
+    //_println("NotExipired");
+  }
+    
+}  
